7e66835af4cedafc351e3b8e93649ebb79d2c4a2
Sayed Saifur Rahman <saifurrahmans306@gmail.com>
2025-03-07 00:01:01 +0600

    Added Lab file and Theory folder

---
+++ Theory/Home.php
@@ -0,0 +1,11 @@
+<html lang="en">
+<head>
+    <title>WEB</title>
+</head>
+<body>
+    <h1>Hello World</h1>
+    <img src="/springwtj/view/ab.jpg" width="400" height="200" alt="Resized Image">
+    
+
+</body>
+</html>
